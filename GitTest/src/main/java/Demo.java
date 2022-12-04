eb417917310cdf3a49023ea7ed5c683c1c2a2f72
Qiu <318990032@qq.com>
2022-12-05 01:24:12 +0800

    First

---
+++ GitTest/src/main/java/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * @author 邱志豪~
+ * @version 1.0
+ * @Date 2022/12/5 1:23
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
